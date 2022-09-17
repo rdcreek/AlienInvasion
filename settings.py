906607c23901327c582af926e12ccbdc9fa893f2
rdcreek <rdcreek2012@gmail.com>
2022-09-17 16:08:46 -0400

    Ship can be moved to the right with each keypress.

--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ class Settings:
         #Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (66, 245, 236)
 
