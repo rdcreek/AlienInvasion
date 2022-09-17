18ec788f48e2686f8af73ca5bbdddee5778019d3
rdcreek <rdcreek2012@gmail.com>
2022-09-17 16:15:37 -0400

    Ship can be moved to the right continuously...but it will fall off of the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,7 +22,10 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     #Move the ship to the right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
@@ -35,6 +38,7 @@ class AlienInvasion:
         #Watch for keyboard/mouse events.
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
 if __name__ == '__main__':
