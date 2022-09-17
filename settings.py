c45c22bc22444c7074dc7efe697be5b252eb6cc0
rdcreek <rdcreek2012@gmail.com>
2022-09-17 02:12:13 -0400

    Changed background color.

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    #This is a class to store all of the settings for the game.
+
+    def __init__(self):
+        #initialize the game's settings
+
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
