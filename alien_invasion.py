2084e6b6e817197f22473819cde36dd2333928dc
rdcreek <rdcreek2012@gmail.com>
2022-10-04 18:35:36 -0400

    Created a row of aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -73,7 +73,7 @@ class AlienInvasion:
         """Update position of bullets and get rid of old bullets."""
         self.bullets.update()
         self._update_screen()
-        # Get rid of bullets that have gone off screen
+        # Get rid of bullets that have gone off-screen
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
@@ -84,6 +84,17 @@ class AlienInvasion:
         #Make an alien.
         alien = Alien(self)
         self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        #Create the first row of aliens.
+        for alien_number in range(number_aliens_x):
+            #Create an alien and place it in the row.
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
 
     def run_game(self):
